9949716d62d431620bd148c195d37ad21f9f0bbe
aWarpingTrick <43278129+aWarpingTrick@users.noreply.github.com>
2019-05-05 01:06:08 -0400

    Update Run_script.bat

--- Run_script.bat
+++ Run_script.bat
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-"C:\Users\warin\Desktop\APIs\Git Hub\twitterProject\venv\Scripts\python.exe" "C:\Users\warin\Desktop\APIs\Git Hub\twitterProject\sql_quick_update.py"
-pause
-||||||| merged common ancestors
-"C:\Users\warin\Desktop\APIs\Git Hub\twitterProject\venv\Scripts\python.exe" "C:\Users\warin\Desktop\APIs\Git Hub\twitterProject\Code.py"
-=======
 "C:\Users\warin\Desktop\APIs\Git Hub\twitterProject\venv\Scripts\python.exe" "C:\Users\warin\Desktop\APIs\Git Hub\twitterProject\Code_wSQL.py"
 pause
->>>>>>> 67d0c5979e703e65c3040e5ae2eac306de64e37c
